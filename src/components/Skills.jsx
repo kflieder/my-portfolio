44b335d1ef4fb6c207dc9d6e7a4c07894c8197e9
kflieder <kacey.flieder@gmail.com>
2024-01-23 19:47:04 -0600

    started about component

---
+++ src/components/Skills.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import HTML from '../assets/html.png';
+import CSS from '../assets/css.png';
+import JavaScript from '../assets/javascript.png';
+import REACT from '../assets/react.png';
+import GITHUB from '../assets/github.png';
+import TAILWIND from '../assets/tailwind.png';
+
+
+export const Skills = () => {
+  return (
+    <div name='skills' className='bg-[#0a192f] text-gray-300 '>
+        {/* container */}
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Experience</p>
+                <p className='py-4'>These are the technologies I've worked with</p>
+            </div>
+            <div className='w-full grid grid-cols-2 sm:grid-cols-4 text-center py gap-8'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={HTML} alt='html icon'/>
+                    <p className='my-4'>HTML</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={CSS} alt='css icon'/>
+                    <p className='my-4'>CSS</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={JavaScript} alt='Javascript icon'/>
+                    <p className='my-4'>JavaScript</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={REACT} alt='react icon'/>
+                    <p className='my-4'>React</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={GITHUB} alt='GitHub icon'/>
+                    <p className='my-4'>GitHub</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={TAILWIND} alt='tailwind icon'/>
+                    <p className='my-4'>Tailwind</p>
+                </div>
+            
+            </div>
+        </div>
+    </div>
+  )
+}
