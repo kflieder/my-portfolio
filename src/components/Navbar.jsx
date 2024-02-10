dcdb57f9f8fe30f307a9e2d4e1f7f2832bd4f697
kflieder <kacey.flieder@gmail.com>
2024-02-10 13:58:36 -0600

    React Scroll
    Added react scroll to main nav and mobile nav.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import mypic from '../assets/mypic.jpeg'
 import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa'
 import { HiOutlineMail } from 'react-icons/hi'
 import {BsFillPersonFill} from 'react-icons/bs'
+import {Link} from 'react-scroll'
 
 export const Navbar = () => {
     const [nav, setNav] = useState(false)
@@ -15,11 +16,46 @@ export const Navbar = () => {
         {/* menu */}
         <div className='hidden md:flex'>
             <ul className='flex'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Work</li>
-                <li>Contact</li>
+                <li>
+                <Link 
+                    to="home"  
+                    smooth={true} 
+                    duration={500}>
+                        Home
+                </Link>
+                </li>
+                <li>
+                <Link 
+                    to="about"  
+                    smooth={true} 
+                    duration={500}>
+                        About
+                </Link>
+                </li>
+                <li>
+                <Link 
+                    to="skills"  
+                    smooth={true} 
+                    duration={500}>
+                        Skills
+                </Link>
+                </li>
+                <li>
+                <Link 
+                    to="work"  
+                    smooth={true} 
+                    duration={500}>
+                        Work
+                </Link>
+                </li>
+                <li>
+                <Link 
+                    to="contact"  
+                    smooth={true} 
+                    duration={500}>
+                        Contact
+                </Link>
+                </li>
             </ul>
         </div>
 
@@ -30,11 +66,46 @@ export const Navbar = () => {
         </div>
         {/* Mobile menu */}
         <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-            <li className='py-6 text-4x1'>Home</li>
-            <li className='py-6 text-4x1'>About</li>
-            <li className='py-6 text-4x1'>Skills</li>
-            <li className='py-6 text-4x1'>Work</li>
-            <li className='py-6 text-4x1'>Contact</li>
+            <li className='py-6 text-4x1'>
+            <Link onClick={handleClick}
+                    to="home"  
+                    smooth={true} 
+                    duration={500}>
+                        Home
+                </Link>
+            </li>
+            <li className='py-6 text-4x1'>
+            <Link onClick={handleClick}
+                    to="about"  
+                    smooth={true} 
+                    duration={500}>
+                        About
+                </Link>
+            </li>
+            <li className='py-6 text-4x1'>
+            <Link onClick={handleClick}
+                    to="skills"  
+                    smooth={true} 
+                    duration={500}>
+                        Skills
+                </Link>
+            </li>
+            <li className='py-6 text-4x1'>
+            <Link onClick={handleClick}
+                    to="work"  
+                    smooth={true} 
+                    duration={500}>
+                        Work
+                </Link>
+            </li>
+            <li className='py-6 text-4x1'>
+            <Link onClick={handleClick}
+                    to="contact"  
+                    smooth={true} 
+                    duration={500}>
+                        Contact
+                </Link>
+            </li>
         </ul>
 
 
